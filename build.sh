d78ebc3f5a14bcbec75021f3256b39153be70d31
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2023-10-01 12:25:15 +0100

    gawk then awk

--- build.sh
+++ build.sh
@@ -151,10 +151,10 @@ meson_wrap_key() {
     # $1 = package shortname
     # $2 = file section
     # $3 = file key
-    awk -F ' *= *' \
-            'BEGIN {want_section="'$2'"; want_key="'$3'"}
-            match($0, /^\[([^]]*)\]$/, out) {section=out[1]}
-            section == want_section && $1 == want_key {print $2}'
+    gawk -F ' *= *' \
+            -e 'BEGIN {want_section="'$2'"; want_key="'$3'"}' \
+            -e 'match($0, /^\[([^]]*)\]$/, out) {section=out[1]}' \
+            -e 'section == want_section && $1 == want_key {print $2}' \
             "meson/subprojects/$(echo $1 | tr _ -).wrap"
 }
 

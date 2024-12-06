304d32d9904f23c720fbf1f282acedbf00c366bf
Alan O'Callaghan <alan.ocallaghan@outlook.com>
2024-12-06 16:24:19 +0000

    Add Linux aarch64 support (#3)
    * Enable linux aarch64
    
    * Gradle snapshot build

--- process-artifacts.sh
+++ process-artifacts.sh
@@ -1,11 +1,12 @@
 mkdir -p build
 pushd build
 
-mkdir -p darwin-aarch64 darwin-x86-64 linux-x86-64 win32-x86-64 licenses
-cp ../artifacts/libopenslide.so linux-x86-64
-cp ../artifacts/libopenslide.dylib darwin-aarch64
-cp ../artifacts/libopenslide.dylib darwin-x86-64
-cp ../artifacts/libopenslide*.dll win32-x86-64/openslide.dll
+mkdir -p darwin-aarch64 darwin-x86-64 linux-x86-64 win32-x86-64 linux-aarch64 licenses
+cp ../artifacts/linux-x86_64/libopenslide.so linux-x86-64
+cp ../artifacts/linux-aarch64/libopenslide.so linux-aarch64
+cp ../artifacts/macos-arm64-x86_64/libopenslide.dylib darwin-aarch64
+cp ../artifacts/macos-arm64-x86_64/libopenslide.dylib darwin-x86-64
+cp ../artifacts/windows-x64/libopenslide*.dll win32-x86-64/openslide.dll
 cp -r ../downloads/openslide-bin-*-linux-x86_64/licenses/* licenses
 cp -r ../downloads/openslide-bin-*-macos-arm64-x86_64/licenses/* licenses
 cp -r ../downloads/openslide-bin-*-windows-x64/licenses/* licenses
@@ -13,7 +14,8 @@ cp -r ../downloads/openslide-bin-*-windows-x64/licenses/* licenses
 jar cvf openslide-natives-darwin-aarch64.jar darwin-aarch64 licenses
 jar cvf openslide-natives-darwin-x86-64.jar darwin-x86-64 licenses
 jar cvf openslide-natives-linux-x86-64.jar linux-x86-64 licenses
+jar cvf openslide-natives-linux-aarch64.jar linux-aarch64 licenses
 jar cvf openslide-natives-win32-x86-64.jar win32-x86-64 licenses
-jar cvf openslide-natives.jar linux-x86-64 darwin-x86-64 darwin-aarch64 win32-x86-64 licenses
+jar cvf openslide-natives.jar linux-x86-64 linux-aarch64 darwin-x86-64 darwin-aarch64 win32-x86-64 licenses
 
 popd
